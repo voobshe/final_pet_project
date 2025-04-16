9833c6c35aa8efb408fed137eb63d926c7236383
analogina <fr.snork@gmail.com>
2025-04-16 21:02:38 +0300

    now telegram bot handle users

--- bot.py
+++ bot.py
@@ -45,29 +45,104 @@ class LisaBot:
         self.application = Application.builder().token(BOT_KEY).build()
 
         self.application.add_handler(CommandHandler("start", LisaBot.start))
+        self.application.add_handler(CommandHandler("clear", LisaBot.clear_data))
+        self.application.add_handler(CommandHandler("calc", LisaBot.calculate))
         self.application.add_handler(MessageHandler(filters.PHOTO, LisaBot.handle_photo))
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, LisaBot.handle_text))
         self.application.bot_data = self
+
+        self.photo_data = list()
+        self.user_data = dict()
+        self.katusha = katusha_part
+        
 
 
     async def start(update: Update, context:CallbackContext)->None:
         """
         This method asks the user to send the price tag of the product.
         """
+
+        user_id = context._user_id
+        reply = ''
+        mb_photo_data = self.user_data.get(user_id)
+        if mb_photo_data == None:
+            self.user_data[user_id] = list()
+            reply = f'Создан новый пользователь с ID {user_id} \U0001F44B \n'
+
         #self.logger.info(f"User {update.effective_user.full_name} started the bot")
-        await update.message.reply_text('Отправь фотографию ценника.')
+        await update.message.reply_text('Отправь фотографию ценников')
+
+
+    async def clear_data(update: Update, context:CallbackContext)->None:
+        """
+        this method clear photo data
+        """
+        self = context.bot_data
+
+        user_id = context._user_id
+        reply = ''
+        mb_photo_data = self.user_data.get(user_id)
+        if mb_photo_data == None:
+            self.user_data[user_id] = list()
+            reply = f'Создан новый пользователь с ID {user_id} \U0001F44B \n'
+
+        reply = f"количество записей в списке: {len(self.user_data[user_id])} \n"
+        reply += "список очищен"
+
+        self.user_data[user_id].clear()
+        #self.logger.info(f"User {update.effective_user.full_name} started the bot")
+        await update.message.reply_text(reply)
+
+    async def calculate(update: Update, context:CallbackContext)->None:
+        """
+        this method calculate optimal price
+        """
+        self = context.bot_data
+
+        user_id = context._user_id
+        reply = ''
+        mb_photo_data = self.user_data.get(user_id)
+        if mb_photo_data == None:
+            self.user_data[user_id] = list()
+            reply = f'Создан новый пользователь с ID {user_id} \U0001F44B \n'
+
+        reply = f"список содержал {len(self.user_data[user_id])} записи\n"
+        # pass user_data[user_id] to katusha.photo_handler
+        photos = self.user_data[user_id]
+        reply += "рассчеты проведены\n"
+        reply += "список url фотографий: \n "
+        for photo_url in photos:
+            reply += f"{photo_url}\n"
+        self.user_data[user_id].clear()
+        #self.logger.info(f"User {update.effective_user.full_name} started the bot")
+        await update.message.reply_text(reply)
+
 
     async def handle_photo(update: Update, context:CallbackContext):
         """
         This method accepts photo.
         """
+        self = context.bot_data
+        user_id = context._user_id
+        reply = ''
+        mb_photo_data = self.user_data.get(user_id)
+        if mb_photo_data == None:
+            self.user_data[user_id] = list()
+            reply = f'Создан новый пользователь с ID {user_id} \U0001F44B \n'
+
         #lisa = context.bot_data
         photo = update.message.photo[-1]
+
         file = await photo.get_file()
+        photo_url = file.file_path
         #file = file.download_file(file_path)
-        await file.download_to_drive("testfile.jpg")
+        self.user_data[user_id].append(photo_url)
+        pd_len = len(self.user_data[user_id])
+        
+        reply += f'Добавлена фотография для сравнения.\n Всего для сравнения {pd_len} фотографий'        
         #self.logger.info(f"Фото получено от: {update.effective_user.full_name}")
-        await update.message.reply_text('Я обрабатываю фотографию ценника')
+        await update.message.reply_text(reply)
+        
 
     async def handle_text(update: Update, context:CallbackContext):
         """
